94276c0c607107afbc8504e3af2d50e50e82255e
mix911 <wmihanale@gmail.com>
2012-03-10 23:58:07 +0400

    Add raw no native tabview

---
+++ XManager/TabsHeaders.h
@@ -0,0 +1,13 @@
+//
+//  TabsHeaders.h
+//  XManager
+//
+//  Created by demo on 10.03.12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TabsHeaders : NSView
+
+@end
---
+++ XManager/TabsHeaders.m
@@ -0,0 +1,28 @@
+//
+//  TabsHeaders.m
+//  XManager
+//
+//  Created by demo on 10.03.12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TabsHeaders.h"
+
+@implementation TabsHeaders
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
